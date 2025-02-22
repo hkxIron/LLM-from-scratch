3f54157e21d48fb7b5a86bb507382910cb669a77
hukexin <hukexin@xiaomi.com>
2025-02-22 16:52:43 +0800

    add

--- TinyStories/scp.sh
+++ TinyStories/scp.sh
@@ -2,4 +2,5 @@ echo "HOME:$HOME"
 # 同步数据
 remote_project_path="/media/hkx/win/hkx/ubuntu/work/open/LLM-from-scratch/"
 local_project_path="$HOME/work/open/project/LLM-from-scratch"
-rsync -av -e ssh --exclude='*.git' --exclude='.*' --exclude='*checkpoints*' --exclude='__pycache__/' --exclude='wandb/' hkx@10.239.6.137:${remote_project_path} $local_project_path
+user_ip="hkx@10.239.6.137"
+rsync -av -e ssh --exclude='*.git' --exclude='.*' --exclude='*checkpoints*' --exclude='__pycache__/' --exclude='wandb/' ${user_ip}:${remote_project_path} $local_project_path
